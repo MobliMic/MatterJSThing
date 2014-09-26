1cd8a7a07403cd8da7af624e2c1bb4a7ee6dd06d
Michael Craddock <michael@moblimic.com>
2014-09-27 00:22:45 +0100

    WHY HAVE MY BORDERS GONE D:

--- resources/js/game/main.js
+++ resources/js/game/main.js
@@ -10,13 +10,19 @@ var Composites = Matter.Composites;
 var MouseConstraint = Matter.MouseConstraint;
 var Events = Matter.Events;
 
+// global variables
+var engine;
+var scene;
+
 var MatterDemo = {};
 
-MatterDemo.init = function() {
+MatterDemo.init = function () {
 
     var container = document.getElementById('main');
 
     var options = [];
+
+    engine = Engine.create(container, options);
 
     var borders = [
         Bodies.rectangle(395, 600, 815, 50, {isStatic: true}),
@@ -25,10 +31,20 @@ MatterDemo.init = function() {
         Bodies.rectangle(0, 300, 50, 815, {isStatic: true})
     ];
 
-    var engine = Engine.create(container, options);
     var mouse = MouseConstraint.create(engine, {
         constraint: {stiffness: 1}
     });
+
+    World.add(engine.world, [mouse, borders]);
+
+    Engine.run(engine);
+
+    scene = 'main';
+
+    MatterDemo[scene]();
+};
+
+MatterDemo.main = function () {
 
     var rock = Bodies.polygon(170, 450, 8, 20);
 
@@ -44,12 +60,9 @@ MatterDemo.init = function() {
     var obstacle = Bodies.rectangle(600, 400, 350, 15, {isStatic: true});
 
     var stack = Composites.stack(550, 400, 1, 5, 0, 0,
-        function (x, y, column, row) {
+        function (x, y) {
             return Bodies.rectangle(x, y, 25, 25);
         });
-
-    World.add(engine.world, [mouse, obstacle, pyramid, stack, rock, elastic]);
-    World.add(engine.world, borders);
 
     Events.on(engine, 'tick', function (event) {
         if (engine.input.mouse.button === -1 && rock.position.x > 190) {
@@ -59,18 +72,15 @@ MatterDemo.init = function() {
         }
     });
 
-    Engine.run(engine);
+    World.add(engine.world, [obstacle, pyramid, stack, rock, elastic]);
 
-};
-
-MatterDemo.main = function() {
 
 };
 
 window.addEventListener('load', MatterDemo.init);
 
-$(document).ready(function(){
-    $( '#main' ).on( "mousemove", function( event ) {
-        $( "#sand" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+$(document).ready(function () {
+    $('#main').on("mousemove", function (event) {
+        $("#sand").text("pageX: " + event.pageX + ", pageY: " + event.pageY);
     });
 });
